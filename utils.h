47da43d45ac89105918aeb979cb9fc6a24197e1b
David Whedon <dwhedon@debian.org>
2002-09-06 03:38:58 +0000

    * remove util.[ch] (functions now in libdebian-installer)
    r928

--- utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef __UTILS_H__
-#define __UTILS_H__
-
-int execlog (char *);
-int check_dir (char *);
-int snprintfcat (char *str, size_t size, const char *format, ...);
-
-#endif /* __UTILS_H__ */
