dbc56424a1ca2c8a41ccd09a7e02ce7e255dff23
Joshua Kwan <joshk@debian.org>
2004-05-25 06:27:45 +0000

    give some credit where credit is due
    r16194

--- netcfg-common.c
+++ netcfg-common.c
@@ -3,6 +3,7 @@
    the debian-installer.
 
    Copyright (C) 2000-2002  David Kimdon <dwhedon@debian.org>
+                            and others (see debian/copyright)
    
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
