6e67fe4e1c5cf4cac36dab827267709b65c63bad
Joshua Kwan <joshk@debian.org>
2004-07-17 08:48:59 +0000

    seed get_domain with the domain cut off by seed_hostname_from_dns
    r17801

--- netcfg-common.c
+++ netcfg-common.c
@@ -692,6 +692,9 @@ void seed_hostname_from_dns (struct debconfclient * client, struct in_addr *ipad
       *ptr = '\0';
 
     debconf_set(client, "netcfg/get_hostname", res->ai_canonname);
+
+    if (!have_domain && (ptr + 1) != '\0')
+      debconf_set(client, "netcfg/get_domain", ptr + 1);
   }
 
   freeaddrinfo(res);
